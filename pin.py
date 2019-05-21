291e4bbad20409e64509238499bf3876b1344c4f
caozilong <caozilong@allwinnertech.com>
2019-05-21 16:16:38 +0800

    update'
    Signed-off-by: caozilong <caozilong@allwinnertech.com>

--- pin.py
+++ pin.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pylab as pl
+
 sampling_rate = 8000
-fft_size = 512
+fft_size = 8000
 t = np.arange(0, 1.0, 1.0/sampling_rate)
-x = np.sin(2*np.pi*156.25*t) + 2*np.sin(2*np.pi*234.375*t)
-# x = np.cos(2 *np.pi * 4000*t)
+x = np.sin(2*np.pi*156.25*t)  + 2*np.sin(2*np.pi*234.375*t)
 xs = x[:fft_size]
-# print("", xs)
-xf = np.fft.rfft(xs)/fft_size
-freqs = np.linspace(0, sampling_rate/2, fft_size/2+1)
+xf = np.fft.fft(xs)*2/fft_size
+print(len(xs))
+print(len(xf))
+freqs = np.linspace(0, sampling_rate, fft_size)
 xfp = 20*np.log10(np.clip(np.abs(xf), 1e-20, 1e100))
 pl.figure(figsize=(8,4))
 pl.subplot(211)
 pl.plot(t[:fft_size], xs)
-pl.xlabel(u"Time(S)")
-pl.title(u"156.25Hz and 234.375Hz WaveForm And Freq")
+pl.xlabel(u"时间(秒)")
+pl.title(u"156.25Hz和234.375Hz的波形和频谱")
 pl.subplot(212)
 pl.plot(freqs, xfp)
-pl.xlabel(u"Freq(Hz)")
+pl.xlabel(u"频率(Hz)")
 pl.subplots_adjust(hspace=0.4)
 pl.show()
